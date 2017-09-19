3654c04f221a0d6cc84084b8f1f93062d598c238
ahw <andrewhallagan@gmail.com>
2017-09-19 08:28:40 -0400

    Basic row and column summations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-import tokenizer from 'string-tokenizer';
-tokenizer.token('digit', /\d+/);
+import { splitToRows, splitToColumns } from './splitter';
 
 class App extends Component {
     onChange(e) {
-        tokenizer.input(e.target.value);
+        const text = e.target.value;
+        const rows = splitToRows(text).map(splitToColumns);
+        const cols = [];
+        const maxCols = rows.reduce((prev, row) => Math.max(row.length, prev), 0);
+        for (let i = 0; i < rows.length; i++) {
+            for (let j = 0; j < rows[i].length; j++) {
+                if (typeof cols[j] === 'undefined') {
+                    cols[j] = [];
+                }
+
+                // console.log(`Pushing item at row ${i} col ${j} to col ${j}`);
+                cols[j].push(rows[i][j]);
+            }
+        }
+
+
+        // Reduce 'em
+        const rowSum = rows.filter(row => row.length).map(arr => {
+            return arr.map(n => parseFloat(n)).reduce((prev, curr) => prev + curr)
+        });
+
+        const colSum = cols.filter(col => col.length).map(arr => {
+            return arr.map(n => parseFloat(n)).reduce((prev, curr) => prev + curr)
+        });
+
+        console.log('row summations', rowSum);
+        console.log('col summations', colSum);
     }
 
     render() {
