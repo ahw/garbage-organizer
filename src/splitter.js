5abf2f4a2477e8d20552520ef95394674e567a56
ahw <andrewhallagan@gmail.com>
2017-09-20 08:07:32 -0400

    Add reducers, splitter modules

---
+++ src/splitter.js
@@ -0,0 +1,8 @@
+const numberRegex = /(\d*\.?\d+)/g;
+export function splitToRows(str) {
+    return str.split('\n');
+}
+
+export function splitToColumns(line) {
+    return line.split(numberRegex).filter(chunk => numberRegex.test(chunk));
+}
