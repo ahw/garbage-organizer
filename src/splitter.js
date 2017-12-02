e92869ce1e0ca926aa4616f2dc84aa7ea96596e7
ahw <andrewhallagan@gmail.com>
2017-12-02 16:43:56 -0500

    Updates

--- src/splitter.js
+++
@@ -1,8 +0,0 @@
-const numberRegex = /(-?\d*\.?\d+)/g;
-export function splitToRows(str) {
-    return str.split('\n');
-}
-
-export function splitToColumns(line) {
-    return line.split(numberRegex).filter(chunk => numberRegex.test(chunk));
-}
